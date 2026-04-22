151b4210f2381ca1395b25f965ddd5c3ae3dc464
Bruno <brutl006@gmail.com>
2026-04-22 17:12:20 -0300

    Hice que se pueda cambiar de modo claro a modo oscuro

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ titulo.onclick = function() {
     titulo.style.backgroundColor = "red";
     alert("Tocaste el titulo!");
 };
+
+let modoOscuro = false;
+const boton = document.getElementById('botonAccion');
+function saludar() {
+if (modoOscuro === false) {
+    document.body.style.backgroundColor = "#2c3e50";
+    titulo.style.color = "white"
+    boton.innerText = "Modo claro"
+    modoOscuro = true;
+} else {
+    document.body.style.backgroundColor = "rgb(224, 224, 224)";
+    titulo.style.color = "black"
+    boton.innerText = "Modo oscuro"
+    modoOscuro = false
+}
+}
+
+boton.onclick = saludar;
